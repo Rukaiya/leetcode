2e06e96c5caa31b8441f0a18312445b1171b6e98
Rukaiya Amin <rukaiyaamin36@gmail.com>
2023-12-01 13:19:18 +0600

    443 serialize issue

--- 443_string_compression.rb
+++ 443_string_compression.rb
@@ -1,35 +1,28 @@
 # @param {Character[]} chars
 # @return {Integer}
 def compress(chars)
-  return 1 if chars.length == 1
+  return chars if chars.length == 1
 
-  s =
+  s = chars[0]
   char_len = chars.length
   count = 1
-  first_index = 0
-  last_index = 0
   (1..(char_len - 1)).each do |i|
     if chars[i] == chars[i - 1]
       count += 1
       s += count.to_s if count > 1 && i == char_len - 1
     else
       s += count.to_s if count > 1
-      if count == 1
-        first_index = i
-      elsif count > 1
-        last_index = i
-        chars.slice!((first_index + 1)...last_index)
-      end
-
+      s += chars[i]
       count = 1
     end
   end
-  chars
+  s.split('')
 end
 
 output = compress(["a","a","b","b","c","c","c"])
-puts output
+puts output.inspect
 output = compress(["a"])
-puts output
+puts output.inspect
 output = compress(["a","b","b","b","b","b","b","b","b","b","b","b","b"])
-puts output
+puts output.inspect
+
